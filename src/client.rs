adb8e163dc1f8718986411d10fef33a1e9ee7e78
David Harkness <dharkness@gmail.com>
2024-06-15 15:01:55 -0700

    Switch from BytesMut to Vec; implement COMMAND and ECHO commands

---
+++ src/client.rs
@@ -0,0 +1,150 @@
+use std::io;
+use std::io::{Read, Write};
+use std::str::from_utf8;
+
+use mio::net::TcpStream;
+use mio::{Interest, Registry, Token};
+use mio::event::Source;
+
+use crate::{interrupted, would_block};
+
+pub struct Client {
+    token: Token,
+    stream: TcpStream,
+    incoming: Vec<u8>,
+    outgoing: Vec<u8>,
+}
+
+impl Client {
+    pub fn new(token: Token, stream: TcpStream) -> Self {
+        Self {
+            token,
+            stream,
+            incoming: Vec::with_capacity(1024),
+            outgoing: Vec::with_capacity(4 * 1024),
+        }
+    }
+
+    pub fn start(&mut self, registry: &Registry) -> io::Result<()> {
+        self.stream.register(registry, self.token, Interest::READABLE)
+    }
+
+    pub fn receive(&mut self, registry: &Registry) -> io::Result<bool> {
+        let mut buf = vec![0; 1024];
+        let mut bytes_read = 0;
+
+        loop {
+            println!("reading up to {} bytes", buf.len() - bytes_read);
+            match self.stream.read(&mut buf[bytes_read..]) {
+                Ok(0) => {
+                    println!("connection closed");
+
+                    self.stream.shutdown(std::net::Shutdown::Both)?;
+                    self.stream.deregister(registry)?;
+                    return Ok(true);
+                }
+                Ok(n) => {
+                    println!("read {} bytes", n);
+
+                    bytes_read += n;
+                    if bytes_read == buf.len() {
+                        buf.resize(buf.len() + 1024, 0);
+                    }
+                }
+                Err(ref err) if would_block(err) => {
+                    println!("read {}", from_utf8(&buf[..bytes_read]).unwrap());
+
+                    self.incoming.extend_from_slice(&buf[..bytes_read]);
+
+                    return Ok(false);
+                },
+                Err(ref err) if interrupted(err) => {
+                    println!("interrupted");
+                },
+                Err(err) => return Err(err),
+            };
+        }
+    }
+
+    pub fn process_command(&mut self, registry: &Registry) -> io::Result<()> {
+        let mut data = from_utf8(&self.incoming).unwrap().to_string();
+
+        if data == "*2\r\n$7\r\nCOMMAND\r\n$4\r\nDOCS\r\n" {
+            println!("docs");
+
+            self.incoming.truncate(0);
+
+            return self.write(b"*0\r\n", registry);
+            // return self.write(b"%2\r\n+PING\r\n%2\r\n+summary\r\n+Play ping-pong\r\n+group\r\n+connection\r\n+ECHO\r\n%2\r\n+summary\r\n+Hear yourself speak\r\n+group\r\n+connection\r\n", registry);
+        }
+
+        if let Some(index) = data.find("ECHO") {
+            let mut cmd = data.split_off(index + 7);
+            if let Some(index) = cmd.find("\r\n") {
+                let echo = cmd.split_off(index + 2);
+                self.write(b"+", registry)?;
+                self.write(echo.as_bytes(), registry)?;
+            }
+        }
+
+        loop {
+            if let Some(index) = data.find("PING") {
+                self.write(b"+PONG\r\n", registry)?;
+                data = data.split_off(index + 4);
+            } else {
+                break
+            }
+        }
+
+        self.incoming.truncate(0);
+        Ok(())
+    }
+
+    pub fn write(&mut self, data: &[u8], registry: &Registry) -> io::Result<()> {
+        self.outgoing.extend_from_slice(data);
+        self.stream.reregister(registry, self.token, Interest::READABLE | Interest::WRITABLE)
+    }
+
+    pub fn send(&mut self, registry: &Registry) -> io::Result<bool> {
+        let mut bytes_sent = 0;
+        let mut bytes_left = self.outgoing.len();
+
+        while bytes_left > 0 {
+            println!("writing up to {} bytes", bytes_left);
+            match self.stream.write(&self.outgoing[bytes_sent..self.outgoing.len()]) {
+                Ok(0) => {
+                    println!("connection closed");
+
+                    self.stream.deregister(registry)?;
+                    return Ok(true);
+                },
+                Ok(n) => {
+                    println!("wrote {} bytes", n);
+
+                    bytes_sent += n;
+                    bytes_left -= n;
+                },
+                Err(ref err) if would_block(err) => {
+                    println!("wrote {}", from_utf8(&self.outgoing[..bytes_sent]).unwrap());
+                    if bytes_left > 0 {
+                        println!("did not write {} bytes", bytes_left);
+                    }
+
+                    return Ok(false);
+                },
+                Err(ref err) if interrupted(err) => {
+                    println!("interrupted");
+                },
+                Err(err) => return Err(err),
+            };
+        }
+
+        println!("wrote {}", from_utf8(&self.outgoing).unwrap());
+        self.outgoing.truncate(0);
+
+        // done writing
+        self.stream.flush()?;
+        self.stream.reregister(registry, self.token, Interest::READABLE)?;
+        Ok(false)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,116 @@
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+mod client;
 
-#[tokio::main]
-async fn main() {
+use std::collections::HashMap;
+use std::io;
+
+use mio::event::Event;
+use mio::net::{TcpListener};
+use mio::{Events, Interest, Poll, Registry, Token};
+
+use client::Client;
+
+const SERVER: Token = Token(0);
+
+fn main() -> io::Result<()> {
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(128);
+
+    let addr = "127.0.0.1:6379".parse().unwrap();
+    let mut server = TcpListener::bind(addr)?;
+
+    poll.registry()
+        .register(&mut server, SERVER, Interest::READABLE)?;
+
+    let mut clients = HashMap::new();
+    let mut unique_token = Token(SERVER.0 + 1);
+
     println!("listening for connections");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
     loop {
-        match listener.accept().await {
-            Ok((mut stream, _)) => {
-                println!("accepted new connection");
-                tokio::spawn(async move {
-                    let mut buf = [0; 1024];
-
-                    loop {
-                        match stream.read(&mut buf).await {
-                            Ok(len) => {
-                                if len == 0 {
-                                    break;
-                                }
-                                // if len >= 4 && &buf[..4] == b"PING" {
-                                    stream.write_all(b"+PONG\r\n").await.expect("write failed");
-                                // }
-                            }
-                            Err(e) => {
-                                println!("error reading: {}", e);
-                                break;
-                            }
-                        }
-                    }
-                });
+        if let Err(err) = poll.poll(&mut events, None) {
+            if interrupted(&err) {
+                continue;
             }
-            Err(e) => {
-                println!("error: {}", e);
+            return Err(err);
+        }
+
+        for event in events.iter() {
+            match event.token() {
+                SERVER => loop {
+                    let (stream, address) = match server.accept() {
+                        Ok((stream, address)) => (stream, address),
+                        Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+                            // no more incoming connections
+                            break;
+                        }
+                        Err(e) => {
+                            return Err(e);
+                        }
+                    };
+
+                    println!("accepted connection from: {}", address);
+
+                    let token = next(&mut unique_token);
+                    let registry = poll.registry();
+                    let mut client = Client::new(token, stream);
+
+                    client.start(registry)?;
+                    clients.insert(token, client);
+                },
+                token => {
+                    let done = if let Some(client) = clients.get_mut(&token) {
+                        handle_connection_event(poll.registry(), client, event)?
+                    } else {
+                        // ignore event for unknown token
+                        false
+                    };
+
+                    if done {
+                        clients.remove(&token);
+                    }
+                }
             }
         }
     }
+}
+
+fn next(current: &mut Token) -> Token {
+    let next = current.0;
+    current.0 += 1;
+    Token(next)
+}
+
+/// Returns `true` if the connection is done.
+fn handle_connection_event(
+    registry: &Registry,
+    client: &mut Client,
+    event: &Event,
+) -> io::Result<bool> {
+    if event.is_writable() {
+        println!("writable");
+        match client.send(registry) {
+            Ok(true) => return Ok(true),
+            Ok(false) => (),
+            Err(err) => return Err(err),
+        };
+    }
+
+    if event.is_readable() {
+        println!("readable");
+        match client.receive(registry) {
+            Ok(true) => return Ok(true),
+            Ok(false) => client.process_command(registry)?,
+            Err(err) => return Err(err),
+        };
+    }
+
+    Ok(false)
+}
+
+fn would_block(err: &io::Error) -> bool {
+    err.kind() == io::ErrorKind::WouldBlock
+}
+
+fn interrupted(err: &io::Error) -> bool {
+    err.kind() == io::ErrorKind::Interrupted
 }
