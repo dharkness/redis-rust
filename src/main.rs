bc023bf0c3c8c13cae9fb5adb7c8d608c986ea7f
David Harkness <dharkness@gmail.com>
2024-06-15 15:01:55 -0700

    Uncomment main code

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 fn main() {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
