a9d3ee16afbd273f47b76e451af34480b5c351d2
David Harkness <dharkness@gmail.com>
2024-06-15 15:01:55 -0700

    Hard-code response to PING command

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write(b"+PONG\r\n").expect("write failed");
             }
             Err(e) => {
                 println!("error: {}", e);
